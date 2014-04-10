99023117a97a8d9f0c4b4a725d0df7bc121855b5
Sameer Rahmani <lxsameer@gnu.org>
2014-04-10 22:13:31 +0430

    js manifest bug fixeD

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,8 @@
 //= require jquery_ujs
 //= require foundation
 //= require turbolinks
-//= require_tree .
+//= require home
 
-$(function(){ $(document).foundation(); });
+$(function(){
+    $(document).foundation();
+});
