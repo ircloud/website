4c53b1bbe21c91c617fd09270b36f7b46672df7b
Sameer Rahmani <lxsameer@gnu.org>
2014-05-25 16:50:20 +0430

    unicorn user changed

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,7 +8,7 @@ timeout 180
 listen "127.0.0.1:9000"
 
 # Spawn unicorn master worker for user apps (group: apps)
-user 'www-data', "www-data"
+user 'stackbox', "stackbox"
 
 # Fill path to your app
 working_directory app_path
